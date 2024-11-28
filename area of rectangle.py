9191409efb81c08f46c552f965891847eff30fd0
harini-012 <harinirajesh0519@gmail.com>
2024-11-28 18:38:00 +0530

    Add files via upload

---
+++ area of rectangle.py
@@ -0,0 +1,17 @@
+# area of the given shapes
+length=int(input("Enter the length of the rectangle"))
+breadth=int(input("Enter the breadth of the rectangle"))
+area_r=length*breadth
+print("The area of the rectangle is ",area_r)
+side=int(input("Enter the side of the square"))
+area_s=side*side
+print("The area of the square is ", area_s)
+tria_breadth=int(input("Enter the breadth of the "))
+tria_height=int(input("Enter the height of the triangle"))
+area_t=(tria_breadth*tria_height)/2
+print("The area of the triangle is ", area_t)
+radius=int(input("Enter the radius of the circle"))
+area_c=3.14*radius*radius
+print("The area circle is ", area_c)
+
+
